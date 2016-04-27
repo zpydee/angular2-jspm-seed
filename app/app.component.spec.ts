823f245b584d90ece8241b6dd2dd8b6ab47910db
zpydee <daveb@interactrdt.com>
2016-04-27 23:29:25 +0200

    add tsconfig to deal with ide typescript issues (i.e. = no red squigglies)

--- app/app.component.spec.ts
+++ app/app.component.spec.ts
@@ -1,5 +1,7 @@
 /// <reference path="../typings/browser.d.ts" />
 
+import {} from 'angular2/testing';
+
 describe('Test', function(){
     it('should pass', function(){
         expect(true).toBe(true);
